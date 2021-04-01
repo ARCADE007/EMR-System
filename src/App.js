6f7f9ca229e2627693d1be0052d435e5b908e89e
Vineet <sharma24vineet@gmail.com>
2021-04-01 20:46:11 +0530

    all refreshed login and another cleaned

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col, Container, Jumbotron } from "react-bootstrap";
 import LeftLogin from "./Components/LeftLogin";
 import StaffAfterSubmit from './Components/StaffAfterSubmit';
 function App() {
     return (
         <Router>
-         <div className = "app" >
-             <Switch>
-             <Route path="/ajadoctor">
-            <StaffAfterSubmit/>
+            <div className="app" >
+                <Switch>
 
-        </Route>
-                 <Route path="/">
-                     <Jumbotron style={{ backgroundImage: "url(image/LoginPage.jpg)", backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: "cover", position: 'relative', height: "100vh" }}>
-            <div className="Login__page" style={{ marginTop: "5%", alignContent: "center" }}>
-                <Container style={{ width: "100rem", alignContent: "left" }}>
-                    <Row xs={1} md={2} >
-                        <Col ><LeftLogin /></Col>
-                    </Row>
-                </Container>
+                    <Route path="/ajadoctor">
+                        <StaffAfterSubmit />
+                    </Route>
 
+                    <Route path="/">
+                        <LeftLogin />
+                    </Route>
+
+                </Switch>
             </div>
-        </Jumbotron>
-        </Route>
-        
-        
-        </Switch>
-        </div>  
         </Router>
     );
 }
--- src/Components/StaffAfterSubmit.js
+++ src/Components/StaffAfterSubmit.js
@@ -5,21 +5,7 @@ import {Row, Col, Container, Form, Button } from "react-bootstrap";
 const StaffAfterSubmit = () => {
     return (
         <div>
-            <Container className = "After__Submit" >
-                    <Row className = "After__Content" >
-                        <Col >
-                        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Control type="text" placeholder="Enter Patient Id" />
-  </Form.Group>
-
-  
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form></Col>
-                    </Row>
-                </Container>
+            staff after submit
          
         </div>
     )
