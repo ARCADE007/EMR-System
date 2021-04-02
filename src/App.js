92f3e7ca88f1d8a4df1122af66c89474bf377e4a
Vineet <sharma24vineet@gmail.com>
2021-04-02 22:21:50 +0530

    Aragon login created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LeftLogin from "./Components/LeftLogin";
-import StaffAfterSubmit from './Components/StaffAfterSubmit';
+import Login from "./Components/Login";
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+import "./assets/scss/argon-design-system-react.scss?v1.1.0";
+
 function App() {
     return (
         <Router>
             <div className="app" >
                 <Switch>
 
-                    <Route path="/ajadoctor">
-                        <StaffAfterSubmit />
-                    </Route>
-
                     <Route path="/">
-                        <LeftLogin />
+                        <Login />
                     </Route>
 
                 </Switch>
