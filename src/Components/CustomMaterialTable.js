63acce2dfe5eac5c15da5f04fc782274c569f81e
Vineet <sharma24vineet@gmail.com>
2021-04-11 11:46:41 +0530

    styling to table finalized

--- src/Components/CustomMaterialTable.js
+++ src/Components/CustomMaterialTable.js
@@ -1,101 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import MaterialTable from "material-table";
 export default function CustomMaterialTable(props) {
+  const [columns, setColumns] = useState([
+    {
+      title: "Name",
+      field: "name",
 
-    const [columns, setColumns] = useState([
-        {
-
-            title: 'Name', field: 'name',
-
-            editComponent: props => (
-                <input
-
-                    type="text"
-                    value={props.value}
-                    onChange={e => props.onChange(e.target.value)}
-                />
-
-            ),
-
-            headerStyle: {
-                backgroundColor: 'rgb(82,95,127,0.6)',
-            }
-
-        },
-
-        {
-            title: 'Surname', field: 'surname', headerStyle: {
-                backgroundColor: 'rgb(82,95,127,0.6)',
-            }
-        },
-        {
-            title: 'Birth Year', field: 'birthYear', type: 'numeric', headerStyle: {
-                backgroundColor: 'rgb(82,95,127,0.6)',
-            }
-        },
-
-        {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-            headerStyle: {
-                backgroundColor: 'rgb(82,95,127,0.6)',
-            }
-        },
-
-
-
-    ]);
-
-    const [data, setData] = useState([
-        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-
-    ]);
-
-
-
-
-
-    return (
-        <MaterialTable
-            style={{ backgroundColor: "rgb(82,95,127,0.6)" }}
-            title="Custom Edit Component Preview"
-            columns={columns}
-            data={data}
-            editable={{
-                onRowAdd: newData =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            setData([...data, newData]);
-
-                            resolve();
-                        }, 1000)
-                    }),
-                onRowUpdate: (newData, oldData) =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            const dataUpdate = [...data];
-                            const index = oldData.tableData.id;
-                            dataUpdate[index] = newData;
-                            setData([...dataUpdate]);
-
-                            resolve();
-                        }, 1000)
-                    }),
-                onRowDelete: oldData =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            const dataDelete = [...data];
-                            const index = oldData.tableData.id;
-                            dataDelete.splice(index, 1);
-                            setData([...dataDelete]);
-
-                            resolve();
-                        }, 1000)
-                    }),
-
-            }}
+      editComponent: (props) => (
+        <input
+          type="text"
+          value={props.value}
+          onChange={(e) => props.onChange(e.target.value)}
         />
-    )
+      ),
+
+      
+    },
+
+    {
+      title: "Surname",
+      field: "surname",
+      
+    },
+    {
+      title: "Birth Year",
+      field: "birthYear",
+      type: "numeric",
+      
+    },
+
+    {
+      title: "Birth Place",
+      field: "birthCity",
+      lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+      
+    },
+  ]);
+
+  const [data, setData] = useState([
+    { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+    { name: "Zerya Betül", surname: "Baran", birthYear: 2017, birthCity: 34 },
+  ]);
+
+  return (
+    <MaterialTable
+      style={{ backgroundColor: "rgb(82,95,127,0.6)" }}
+      title="Custom Edit Component Preview"
+      columns={columns}
+      data={data}
+      options={{
+        headerStyle: { backgroundColor: "rgb(82,95,127,0.6)", color: "white" },
+      }}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+
+              resolve();
+            }, 1000);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.id;
+              dataUpdate[index] = newData;
+              setData([...dataUpdate]);
+
+              resolve();
+            }, 1000);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+
+              resolve();
+            }, 1000);
+          }),
+      }}
+    />
+  );
 }
