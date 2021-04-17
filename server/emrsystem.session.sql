1d3e5fff3cefc849b7a94df5a3bca405801ef96e
priyak281 <63048165+priyak281@users.noreply.github.com>
2021-04-17 20:27:40 +0530

    changed name

---
+++ server/emrsystem.session.sql
@@ -0,0 +1,69 @@
+-- Create Queries
+
+CREATE TABLE staff (
+    staffID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    password VARCHAR(20) NOT NULL,
+    staffName VARCHAR(20) NOT NULL,
+    staffPhoneNo INTEGER NOT NULL,
+    staffEmail VARCHAR(20) NOT NULL,
+    staffAddress VARCHAR(30)NOT NULL,
+    PRIMARY KEY(staffID),
+    rollName VARCHAR(20)
+)AUTO_INCREMENT=1;
+
+CREATE TABLE Patient(
+    patientID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT ,
+    password VARCHAR(20) NOT NULL,
+    patientName VARCHAR(20) NOT NULL,
+    patientPhoneNo INTEGER NOT NULL,
+    patientEmail VARCHAR(20) NOT NULL,
+    patientAge INTEGER NOT NULL,
+    patientDOB date NOT NULL,
+    patientAddress Varchar(40) NOT NULL,
+    PRIMARY KEY(patientID)
+)AUTO_INCREMENT=1;
+
+CREATE TABLE Record(
+    recordID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT ,
+    recordName VARCHAR(20) NOT NULL,
+    PRIMARY KEY(recordID),
+    patientID INTEGER UNSIGNED,
+    FOREIGN KEY Record(patientID) REFERENCES emrsystem.Patient(patientID)
+)AUTO_INCREMENT=1;
+
+CREATE TABLE Reports(
+    reportID INTEGER  UNSIGNED NOT NULL AUTO_INCREMENT ,
+    reportName VARCHAR(20) NOT NULL,
+    Date date NOT NULL,
+    File BLOB NOT NULL ,
+    PRIMARY KEY(reportID),
+    recordID INTEGER UNSIGNED,
+    FOREIGN KEY Reports(recordID) REFERENCES emrsystem.Record(recordId)
+)AUTO_INCREMENT=1;
+
+CREATE TABLE Prescription(
+    prescriptionID INTEGER  UNSIGNED NOT NULL AUTO_INCREMENT,
+    description BLOB NOT NULL,
+    disease VARCHAR(40) NOT NULL,
+    PRIMARY KEY(prescriptionID),
+    staffID INTEGER UNSIGNED,
+    patientID INTEGER UNSIGNED,
+    FOREIGN KEY (staffID) REFERENCES emrsystem.staff(staffID),
+    FOREIGN KEY (patientID) REFERENCES emrsystem.Patient(patientID)
+)AUTO_INCREMENT=1;
+
+CREATE TABLE Medicine(
+    medicineId INTEGER  UNSIGNED NOT NULL AUTO_INCREMENT,
+    Name  VARCHAR(20) NOT NULL,
+    `Date(from)` date NOT NULL,
+    `Date(to)` date NOT NULL,
+    `Time(morning)` VARCHAR(20) NOT NULL,
+    `Time(evening)`VARCHAR(20) NOT NULL,
+    `Time(night)` VARCHAR(20) NOT NULL,
+    PRIMARY KEY(medicineID),
+    prescriptionID INTEGER UNSIGNED,
+    FOREIGN KEY Medicine(prescriptionID) REFERENCES emrsystem.Prescription(prescriptionID)
+)AUTO_INCREMENT=1;
+
+-- Insert Queries
+
